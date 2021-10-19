57b3af20fdc56fe1c07760593229f22aa0bafffa
Ramya-cloud-hub <ramya118gowda@gmail.com>
2021-10-19 15:53:23 +0200

    Changes _Index.cshtml

--- Products-Inc/Data/ApplicationDbContext.cs
+++ Products-Inc/Data/ApplicationDbContext.cs
@@ -8,39 +8,12 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Products_Inc.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<User>
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
-        public DbSet<Product> Products { get; set; }
-
 
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         { }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
 
-            IdentityRole roleAdmin = new IdentityRole()
-            {
-                Id = "438db5c8-0513-43a0-a84c-cd416c4e3a54",
-                Name = "Admin",
-                NormalizedName = "ADMIN"
-            };
-            IdentityRole roleUser = new IdentityRole()
-            {
-                Id = "0948bea6-fb82-49c9-8cd8-fec213fe8e8a",
-                Name = "User",
-                NormalizedName = "USER"
-            };
-
-            builder.Entity<IdentityRole>().HasData(
-              roleAdmin, roleUser);
-        }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
@@ -48,78 +21,49 @@ namespace Products_Inc.Data
 
 
 
-
-        public DbSet<User> Users { get; set; }
-        /*public DbSet<Product> Products { get; set; }
-        public DbSet<Country> Countries { get; set; }
-        public DbSet<Language> Languages { get; set; }
-        public DbSet<PersonLanguage> PersonLanguages { get; set; }*/
-
-
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-/*
             // Setting Primarykeys, instead of [Key] in code. One place to handle all of it /ER
-            modelBuilder.Entity<Person>()
-                .HasKey(mb => mb.PersonId);
+            modelBuilder.Entity<Product>()
+                .HasKey(mb => mb.ProductId);
             //.HasName("PrimaryKey_PersonId"); // for reference that i CAN change the name /ER
 
-            modelBuilder.Entity<City>()
-                .HasKey(mb => mb.CityId);
-
-            modelBuilder.Entity<Country>()
-                .HasKey(mb => mb.CountryId);
-
-            modelBuilder.Entity<Language>()
-                .HasKey(mb => mb.LanguageId);
+            modelBuilder.Entity<Order>()
+                .HasKey(mb => mb.OrderId);
 
 
 
-
-            // Setting up One-to-Many
+            /* Setting up One-to-Many
             modelBuilder.Entity<Person>()
                  .HasOne(mbo => mbo.City);
-
             modelBuilder.Entity<City>()
                  .HasMany(mbm => mbm.People);
-
-
-
             modelBuilder.Entity<City>()
                 .HasOne(mbo => mbo.Country);
-
             modelBuilder.Entity<Country>()
                 .HasMany(mbm => mbm.Cities);
+            */
 
 
-            /*modelBuilder.Entity<Person>()
-                 .HasMany(p => p.Languages);
 
-            modelBuilder.Entity<Language>()
-                 .HasMany(la => la.People);*/
-
-
-/*
             // Setting up the join-table for the mutual many-to-many bind/relationship
-            modelBuilder.Entity<PersonLanguage>()  // EF Core 3.x specific. Join table /ER
-                .HasKey(pl => new { pl.PersonId, pl.LanguageId });
+            modelBuilder.Entity<OrderProduct>()  // EF Core 3.x specific. Join table
+                .HasKey(pl => new { pl.OrderId, pl.ProductId });
 
-            modelBuilder.Entity<PersonLanguage>() // One Person to Many Languages
-                .HasOne(ec => ec.Person)
-                .WithMany(e => e.PersonLanguages)
-                .HasForeignKey(ec => ec.PersonId);
+            modelBuilder.Entity<OrderProduct>() // One to Many
+                .HasOne(ec => ec.Product)
+                .WithMany(e => e.OrderProducts)
+                .HasForeignKey(ec => ec.OrderId);
 
-            modelBuilder.Entity<PersonLanguage>()  // One Language to Many People
-                .HasOne(ec => ec.Language)
-                .WithMany(c => c.PersonLanguages)
-                .HasForeignKey(ec => ec.LanguageId);
-*/
+            modelBuilder.Entity<OrderProduct>()  // One  to Many
+                .HasOne(ec => ec.Order)
+                .WithMany(c => c.OrderProducts)
+                .HasForeignKey(ec => ec.ProductId);
+
         }
-
-
-
     }
+
+
 }
--- Products-Inc/Startup.cs
+++ Products-Inc/Startup.cs
@@ -38,7 +38,7 @@ namespace Products_Inc
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("ProductIncConnection")));
            
              
 
